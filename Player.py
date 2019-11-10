25a0ee525156464b69fbd80e8b8406017c7df123
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-10 18:58:13 -0500

    Added changed_direction method.

--- Player.py
+++ Player.py
@@ -2,11 +2,13 @@ class Player:
 
     def __init__(self):
         self.direction = 0
-    
+        self.head_color = (255,0,0)
+        self.body_color = (0,255,255)
+        self.position = [20,20]
 
     def move(self):
         '''Causes snake to move'''
-        pass
+        return self.position
 
     def add_tail(self):
         '''Adds pixel to the end of the snake after eating an apple''' # maybe part of App class?
@@ -17,5 +19,20 @@ class Player:
         pass
 
     def change_direction(self):
-        '''Changes direction upon keypress'''
-        pass
+        '''Changes direction upon keypress
+        0 = right
+        1 = down
+        2 = right
+        3 = up
+        '''
+        
+        if key press to right:
+            self.direction = 0
+        elif key press to down:
+            self.direction = 1
+        elif key press to right:
+            self.direction = 2
+        elif key press to up:
+            self.direction = 3
+
+        return self.direction
