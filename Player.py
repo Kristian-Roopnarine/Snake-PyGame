10838ee62e0f7d0f50e9193c3804f2bc306f6f37
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-10 18:50:50 -0500

    Created methods for Player class.

---
+++ Player.py
@@ -0,0 +1,21 @@
+class Player:
+
+    def __init__(self):
+        self.direction = 0
+    
+
+    def move(self):
+        '''Causes snake to move'''
+        pass
+
+    def add_tail(self):
+        '''Adds pixel to the end of the snake after eating an apple''' # maybe part of App class?
+        pass
+
+    def end(self):
+        '''Checks whether or not the head of the snake hits the body'''
+        pass
+
+    def change_direction(self):
+        '''Changes direction upon keypress'''
+        pass
