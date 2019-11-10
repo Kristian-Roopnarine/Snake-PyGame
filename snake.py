499db7a33d65f1e8536912e9eb9e402ab48333be
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-10 13:14:13 -0500

    Created a grid on PyGame window.

--- snake.py
+++ snake.py
@@ -2,6 +2,23 @@ import pygame
 
 pygame.init()
 
-size = width,height = 400,400
-screen = pygame.display.set_mode(size)
-pygame.display.flip()
+width = 500
+height = 500
+screen = pygame.display.set_mode((width,height))
+
+block_size = 10
+color = (255,255,255)
+running = True
+
+for y in range(height):
+    for x in range(width):
+        rect = pygame.Rect(y * (block_size + 1), x * (block_size + 1), block_size,block_size)
+        pygame.draw.rect(screen,color,rect)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+
