6d632a6466510c13a6e409cb36dfa30459ba1629
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-10 19:03:15 -0500

    Added move() method to Player class.

--- Player.py
+++ Player.py
@@ -4,11 +4,22 @@ class Player:
         self.direction = 0
         self.head_color = (255,0,0)
         self.body_color = (0,255,255)
-        self.position = [20,20]
+        self.headx = 20
+        self.heady = 20
+        self.speed = 5
 
     def move(self):
         '''Causes snake to move'''
-        return self.position
+        if self.direction == 0:
+            self.headx += self.speed
+        elif self.direction == 1:
+            self.heady += self.speed
+        elif self.direction == 2:
+            self.headx -= self.speed
+        elif self.direction == 3:
+            self.heady -= self.speed
+
+        return self.headx,self.heady
 
     def add_tail(self):
         '''Adds pixel to the end of the snake after eating an apple''' # maybe part of App class?
@@ -22,15 +33,15 @@ class Player:
         '''Changes direction upon keypress
         0 = right
         1 = down
-        2 = right
+        2 = left
         3 = up
         '''
-        
+
         if key press to right:
             self.direction = 0
         elif key press to down:
             self.direction = 1
-        elif key press to right:
+        elif key press to left:
             self.direction = 2
         elif key press to up:
             self.direction = 3
