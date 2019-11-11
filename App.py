8a042d754c6ea3cfa8b75442d58e33580dcd9169
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-11 10:28:40 -0500

    Created App class and all methods.

---
+++ App.py
@@ -0,0 +1,28 @@
+import pygame
+
+class App:
+
+    def __init__(self):
+        self.width = 600
+        self.height = 600
+        self.player = Player()
+        self.block = 10
+        self.background = (0,0,0)
+    
+    def drawSnake(self):
+        pass
+
+    def drawGame(self):
+        pass
+
+    def updateGame(self):
+        pass
+
+    def drawApple(self):
+        pass
+
+    def startApp(self):
+        pass
+
+    def endApp(self):
+        pass
